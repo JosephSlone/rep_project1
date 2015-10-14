0aee56d0660cbd796926f2c5063b798d0e94dbbb
Joseph Slone <joe.slone@gmail.com>
2015-10-14 15:10:10 -0400

    Code for Sections 1 and 2

--- commands.R
+++ commands.R
@@ -1,10 +1,35 @@
 library(data.table)
 library(dplyr)
+library(ggplot2)
 dt <- data.table(read.csv("activity.csv"))
 
+#1
 
-# just playing around a bit
-max(dt$steps, na.rm=TRUE)
-# 806
-min(dt$steps, na.rm=TRUE)
-# 0
+dt %>% select(date, steps) %>%
+    group_by(date) %>%
+    summarise(nsteps = sum(steps, na.rm=FALSE)) %>%
+    filter(!is.na(nsteps)) -> steps
+
+rng = range(steps$nsteps)
+ggplot(data=steps, aes(steps$nsteps)) +
+    geom_histogram(breaks=seq(rng[1], rng[2], by=1000), fill="Brown") +
+    xlab("Steps Per Day") +
+    ylab("Total")
+    ggtitle("Total Steps Taken Per Day")
+
+mean(steps$nsteps)
+median(steps$nsteps)
+
+#2
+
+ts <- tapply(dt$steps, dt$interval, mean, na.rm = TRUE)
+plot(row.names(ts), ts, type = "l",
+    xlab = "5-minute interval", ylab = "Average across all Days",
+    main = "Average number of steps taken",
+    col = "Blue")
+
+interval <- which.max(ts)
+names(interval)
+
+
+
